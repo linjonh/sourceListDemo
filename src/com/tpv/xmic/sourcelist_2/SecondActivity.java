da7b777d4f9cfb6757cd33fe8075185add486e08
linjonh <linjonh@gmail.com>
2014-06-04 11:52:11 +0800

    布局每个Item增加三个菜单，初始化全部隐藏，事件还未处理。 控件变量比较多

--- src/com/tpv/xmic/sourcelist_2/SecondActivity.java
+++ src/com/tpv/xmic/sourcelist_2/SecondActivity.java
@@ -3,12 +3,17 @@ package com.tpv.xmic.sourcelist_2;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.View.OnFocusChangeListener;
+import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.RelativeLayout.LayoutParams;
+import android.widget.LinearLayout.LayoutParams;
+import android.widget.PopupWindow;
+import android.widget.TextView;
 
-public class SecondActivity extends Activity implements OnFocusChangeListener {
+public class SecondActivity extends Activity implements OnFocusChangeListener, OnClickListener {
 	private ImageView		iv_1;
 	private ImageView		iv_2;
 	private ImageView		iv_3;
@@ -16,10 +21,58 @@ public class SecondActivity extends Activity implements OnFocusChangeListener {
 	private ImageView		iv_5;
 	private ImageView		iv_6;
 	private ImageView		iv_7;
+	private Button			mRename1;
+	private Button			mRest1;
+	private Button			mSetHome1;
+
+	private Button			mRename2;
+	private Button			mRest2;
+	private Button			mSetHome2;
+
+	private Button			mRename3;
+	private Button			mRest3;
+	private Button			mSetHome3;
+
+	private Button			mRename4;
+	private Button			mRest4;
+	private Button			mSetHome4;
+
+	private Button			mRename5;
+	private Button			mRest5;
+	private Button			mSetHome5;
+
+	private Button			mRename6;
+	private Button			mRest6;
+	private Button			mSetHome6;
+
+	private Button			mRename7;
+	private Button			mRest7;
+	private Button			mSetHome7;
+	private Button[][]		arrBtn		= { { mRename1, mRename2, mRename3, mRename4, mRename5, mRename6, mRename7 },
+			{ mRest1, mRest2, mRest3, mRest4, mRest5, mRest6, mRest7 },
+			{ mSetHome1, mSetHome2, mSetHome3, mSetHome4, mSetHome5, mSetHome6, mSetHome7 } };
+	private int[][]			arrBtnId	= {
+			{ R.id.rename_btn_1, R.id.rename_btn_2, R.id.rename_btn_3, R.id.rename_btn_4, R.id.rename_btn_5, R.id.rename_btn_6, R.id.rename_btn_7 },
+			{ R.id.rest_1, R.id.rest_2, R.id.rest_3, R.id.rest_4, R.id.rest_5, R.id.rest_6, R.id.rest_7 },
+			{ R.id.set_home_page_1, R.id.set_home_page_2, R.id.set_home_page_3, R.id.set_home_page_4, R.id.set_home_page_5, R.id.set_home_page_6,
+			R.id.set_home_page_7 }		};
+
+	private int[]			mTv_id		= { R.id.signal_text_1, R.id.signal_text_2, R.id.signal_text_3, R.id.signal_text_4, R.id.signal_text_5,
+			R.id.signal_text_6, R.id.signal_text_7, };
+	private TextView		mTv_1;
+	private TextView		mTv_2;
+	private TextView		mTv_3;
+	private TextView		mTv_4;
+	private TextView		mTv_5;
+	private TextView		mTv_6;
+	private TextView		mTv_7;
+	private TextView[]		arrTextVew	= { mTv_1, mTv_2, mTv_3, mTv_4, mTv_5, mTv_6, mTv_7, };
+	private boolean			mFocusLeave;
 	// private ImageView Img_test_1;
 	// private ImageView Img_test_2;
 	private int				mScaleInt	= 40;
 	private LayoutParams	origin;
+	private PopupWindow		mPopupWindow;
 
 	public SecondActivity() {
 		// TODO Auto-generated constructor stub
@@ -31,14 +84,13 @@ public class SecondActivity extends Activity implements OnFocusChangeListener {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.fragment_main);
 		getViewReference();
-		registListener();
+		registFocusListener();
+		registClickListenr();
 		setImageScale(iv_1, true);// set first view ,because it won't call
 									// onFocusChange() first of all
 	}
 
 	private void getViewReference() {
-		// Img_test_1 = (ImageView) this.findViewById(R.id.img_test_1);
-		// Img_test_2 = (ImageView) this.findViewById(R.id.img_test_2);
 
 		iv_1 = (ImageView) this.findViewById(R.id.img_one);
 		iv_2 = (ImageView) this.findViewById(R.id.img_two);
@@ -48,11 +100,47 @@ public class SecondActivity extends Activity implements OnFocusChangeListener {
 		iv_6 = (ImageView) this.findViewById(R.id.img_6);
 		iv_7 = (ImageView) this.findViewById(R.id.img_7);
 
+		// mRename1 = (Button) this.findViewById(R.id.rename_btn_1);
+		// mRename2 = (Button) this.findViewById(R.id.rename_btn_2);
+		// mRename3 = (Button) this.findViewById(R.id.rename_btn_3);
+		// mRename4 = (Button) this.findViewById(R.id.rename_btn_4);
+		// mRename5 = (Button) this.findViewById(R.id.rename_btn_5);
+		// mRename6 = (Button) this.findViewById(R.id.rename_btn_6);
+		// mRename7 = (Button) this.findViewById(R.id.rename_btn_7);
+		// mRest1 = (Button) this.findViewById(R.id.rest_1);
+		// mRest2 = (Button) this.findViewById(R.id.rest_2);
+		// mRest3 = (Button) this.findViewById(R.id.rest_3);
+		// mRest4 = (Button) this.findViewById(R.id.rest_4);
+		// mRest5 = (Button) this.findViewById(R.id.rest_5);
+		// mRest6 = (Button) this.findViewById(R.id.rest_6);
+		// mRest7 = (Button) this.findViewById(R.id.rest_7);
+		// mSetHome1 = (Button) this.findViewById(R.id.set_home_page_1);
+		// mSetHome2 = (Button) this.findViewById(R.id.set_home_page_2);
+		// mSetHome3 = (Button) this.findViewById(R.id.set_home_page_3);
+		// mSetHome4 = (Button) this.findViewById(R.id.set_home_page_4);
+		// mSetHome5 = (Button) this.findViewById(R.id.set_home_page_5);
+		// mSetHome6 = (Button) this.findViewById(R.id.set_home_page_6);
+		// mSetHome7 = (Button) this.findViewById(R.id.set_home_page_7);
+		// 全部初始化不可见
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 7; j++) {
+				arrBtn[i][j] = (Button) this.findViewById(arrBtnId[i][j]);
+				if (arrBtn[i][j] != null) {
+					arrBtn[i][j].setVisibility(View.GONE);
+					Log.i("arrBtn", "View.GONE" + arrBtn[i][j].toString());
+				}
+			}
+		}
+
+		for (int i = 0; i < arrTextVew.length; i++) {
+			arrTextVew[i] = (TextView) this.findViewById(mTv_id[i]);
+			arrTextVew[i].setText("signal " + (i + 1));
+		}
+
 	}
 
-	private void registListener() {
-		// Img_test_1.setOnFocusChangeListener(this);
-		// Img_test_2.setOnFocusChangeListener(this);
+	private void registFocusListener() {
+
 		iv_1.setOnFocusChangeListener(this);
 		iv_2.setOnFocusChangeListener(this);
 		iv_3.setOnFocusChangeListener(this);
@@ -60,6 +148,11 @@ public class SecondActivity extends Activity implements OnFocusChangeListener {
 		iv_5.setOnFocusChangeListener(this);
 		iv_6.setOnFocusChangeListener(this);
 		iv_7.setOnFocusChangeListener(this);
+	}
+
+	private void registClickListenr() {
+		iv_1.setOnClickListener(this);
+		iv_2.setOnClickListener(this);
 	}
 
 	@Override
@@ -71,8 +164,8 @@ public class SecondActivity extends Activity implements OnFocusChangeListener {
 	/**
 	 * 布局是一个RelativeLayout作为一个Item，里面包含ImageView和一个TextView，
 	 * 这样只获取ImageView来实现放大和焦点变化。
-	 * 这种布局的特点必须是Item控件（即RelativeLayout）的宽高不能是固定值或match_parent的值（如果这样的设置会出现不放大或反而变得很小），
-	 * 需要wrap_content的属性值，这样ImageView放大后父控件也会跟着放大。
+	 * 这种布局的特点必须是Item控件（即RelativeLayout）的宽高不能是固定值或match_parent的值
+	 * （如果这样的设置会出现不放大或反而变得很小）， 需要wrap_content的属性值，这样ImageView放大后父控件也会跟着放大。
 	 * 此处的放大思想是款和高都加上一个缩放值。
 	 * 
 	 * @author john.lin
@@ -91,7 +184,79 @@ public class SecondActivity extends Activity implements OnFocusChangeListener {
 			Log.i("setImageScale", "Not hasFocus");
 			LayoutParams params = new LayoutParams(origin.width - mScaleInt, origin.height - mScaleInt);
 			v.setLayoutParams(params);
+			if (mFocusLeave) {
+
+				if (mRename1 != null)
+					this.mRename1.setVisibility(View.GONE);
+				if (mRest1 != null)
+					this.mRest1.setVisibility(View.GONE);
+				if (mSetHome1 != null)
+					this.mSetHome1.setVisibility(View.GONE);
+			}
 		}
 	}
 
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		// View view=View.inflate(this, R.layout.item_pop_window, null);
+		// mPopupWindow = new PopupWindow(view, 600, 400, true);
+		// mPopupWindow.setOnDismissListener(new OnDismissListener() {
+		//
+		// @Override
+		// public void onDismiss() {
+		// // TODO Auto-generated method stub
+		// Log.i("mPopupWindow", "已经执行退出方法");
+		// }
+		// });
+		// Button btn1=(Button) view.findViewById(R.id.rest);
+		// Button btn2=(Button) view.findViewById(R.id.set_home_page);
+		// Button btn3=(Button) view.findViewById(R.id.rename_btn);
+		// OnClickListener clickListener = new OnClickListener() {
+		//
+		// @Override
+		// public void onClick(View v) {
+		// // TODO Auto-generated method stub
+		// mPopupWindow.dismiss();
+		// }
+		// };
+		// btn1.setOnClickListener(clickListener);//TODO 暂时只设置退出popupWindow事件
+		// btn2.setOnClickListener(clickListener);
+		// btn3.setOnClickListener(clickListener);
+
+		switch (v.getId()) {
+		case R.id.img_one:
+			// mPopupWindow.showAsDropDown(v, (int) v.getX(), (int) v.getY());
+			break;
+		case R.id.img_two:
+			// mPopupWindow.showAsDropDown(v, (int) v.getX(), (int) v.getY());
+
+			// 使用弹出菜单
+			// PopupMenu popupMenu=new PopupMenu(this, iv_1);
+			// popupMenu.inflate(R.menu.main);
+
+			// popupMenu.show();
+			break;
+
+		default:
+			break;
+		}
+	}
+
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		// TODO Auto-generated method stub
+		if (keyCode == KeyEvent.KEYCODE_DPAD_RIGHT || keyCode == KeyEvent.KEYCODE_DPAD_LEFT) {
+			mFocusLeave = true;
+		} else {
+			mFocusLeave = false;
+		}
+		// if (keyCode == KeyEvent.KEYCODE_BACK) {
+		// if (mPopupWindow != null)
+		// mPopupWindow.dismiss();
+		// return true;
+		// }
+
+		return super.onKeyDown(keyCode, event);
+	}
 }
